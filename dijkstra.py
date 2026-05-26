2a0350957ff09de654bf8a3b977b63e6d3681b28
SnehaFarhana <farhanasadia2050@gmail.com>
2026-05-26 23:38:38 +0600

    dijkstra updated

--- dijkstra.py
+++ dijkstra.py
@@ -1,1 +1,6 @@
-print("dijkstra initialized")
+def dijkstra_path(grid,start,goal): 
+
+    dist={start:0}; #store cheapest path cost
+    prev={start:None}; #store prev node
+    pq=[(0,start)]; #priority queue (cost(cheapest),node)
+    expanded=0 #counting visited nodes
