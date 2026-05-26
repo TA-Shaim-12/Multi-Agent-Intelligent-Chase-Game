823db45ee34e733db12b2ccaae27d9a852ece679
SnehaFarhana <farhanasadia2050@gmail.com>
2026-05-26 23:52:30 +0600

    dijkstra updated

--- dijkstra.py
+++ dijkstra.py
@@ -4,3 +4,7 @@ def dijkstra_path(grid,start,goal):
     prev={start:None}; #store prev node
     pq=[(0,start)]; #priority queue (cost(cheapest),node)
     expanded=0 #counting visited nodes
+
+    while pq: #iterate till no nodes are left to visit
+        d,cur=heapq.heappop(pq);  #explores the current cheapest path and removes the lowest costing node. cur= current node, d= cur total path cost
+        expanded+=1 #increase count
